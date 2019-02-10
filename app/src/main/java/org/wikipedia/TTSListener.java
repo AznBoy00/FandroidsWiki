9dc373edf217f0c79e97c285f5cee10d0029e405
vincent.sun870530@gmail.com <vincent.sun870530@gmail.com>
2019-02-10 00:38:48 -0500

    #23 Create TTS function inside of PageFragment

---
+++ app/src/main/java/org/wikipedia/TTSListener.java
@@ -0,0 +1,30 @@
+package org.wikipedia;
+
+
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+
+import java.util.Locale;
+
+/*
+public class TTSListener implements TextToSpeech.OnInitListener {
+    private final String TAG = "TTSListener";
+    @Override
+    public void onInit(int status) {
+        // TODO Auto-generated method stub
+        if (status == TextToSpeech.SUCCESS) {
+             int supported = mSpeech.setLanguage(Locale.US);
+//                if ((supported != TextToSpeech.LANG_AVAILABLE) && (supported != TextToSpeech.LANG_COUNTRY_AVAILABLE)) {
+//                    Toast.makeText(MainActivity.this, "不支持当前语言！", Toast.LENGTH_SHORT).show();
+//                    Log.i(TAG, "onInit: 支持当前选择语言");
+//                }else{
+//
+//                }
+            Log.i(TAG, "onInit: TTS引擎初始化成功");
+        }
+        else{
+            Log.i(TAG, "onInit: TTS引擎初始化失败");
+        }
+
+}}
+*/
