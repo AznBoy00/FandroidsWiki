b8d4eb0b06a5fde8d9988cf6d21fccbb32e753ca
lokevin88 <lo_kevin@outlook.com>
2019-02-13 18:11:47 -0500

    updated

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -9,6 +9,6 @@ for file in $REPORT; do
 done
 
 wget -O - -q "https://codecov.io/bash" > .codecov
-chmod +x .codecov
+sudo chmod +x .codecov
 ./.codecov -t $CODECOV_TOKEN
 echo "Uploaded code coverage"
