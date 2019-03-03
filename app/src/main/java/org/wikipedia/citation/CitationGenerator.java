553063f3c4120ce7a8cd5330170c0d96ddd631b8
TheWolfOfTheNorth <davebhardwaj1@gmail.com>
2019-03-03 18:55:56 -0500

    Generate String

--- app/src/main/java/org/wikipedia/citation/CitationGenerator.java
+++ app/src/main/java/org/wikipedia/citation/CitationGenerator.java
@@ -1,16 +1,30 @@
 package org.wikipedia.citation;
 
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.wikipedia.citation.CitationStyle;
+import org.wikipedia.page.PageBackStackItem;
+
+import java.util.Calendar;
 
 public class CitationGenerator {
     private String outPutString;
     private final String _WIKIPEDIA = "Wikipedia";
     private String citationDate;
-    private String link;
+    private String mobileURL;
+    private String webURL;
     private CitationStyle citStyle;
     private String textFormat;
+    private String currentMonth;
+    private String currentDay;
+    private String curentYear;
+    private String currentDate;
+    private String citeURL;
+    private String link;
+
 
     public CitationGenerator(){
         outPutString = "";
@@ -27,6 +41,29 @@ public class CitationGenerator {
         this.link = "";
         textFormat = "";
         citStyle = CitationStyle.APA;
+    }
+
+    public CitationGenerator (PageBackStackItem item){
+        mobileURL = item.getTitle().getMobileUri().toString();
+        webURL = item.getTitle().getCanonicalUri().toString();
+        setDateTime();
+    }
+
+    public void setDateTime() {
+        DateFormat df = new SimpleDateFormat("d MMM yyyy");
+        this.currentDate = df.format(Calendar.getInstance().getTime());
+    }
+
+    public String IEEECitationGenerator(String ArticleTitle){
+        String citeBuilder = "";
+        citeBuilder += "\"" + ArticleTitle + ",\"";
+        citeBuilder += " Wikipedia,";
+        citeBuilder += currentDate;
+        citeBuilder += ". [Online]. Available: ";
+        citeBuilder += mobileURL;
+        citeBuilder += ". [Accessed: " + currentDate + "].";
+
+        return citeBuilder;
     }
 
     public void setCitationDate(String date)
