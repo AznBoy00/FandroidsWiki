d83c1b654c45b6bf391fd39313bf456ffe82686f
Nizar Belhassan <m.nizar.belhassan@gmail.com>
2019-03-03 22:40:18 -0500

    Issue #42 Created a channel for notifications
    - Updated the manifest.xml.
    - Added a Channel for the notifications and changed/enhanced the previous code.

--- app/src/main/java/org/wikipedia/notifications/NotificationRandomArticle.java
+++ app/src/main/java/org/wikipedia/notifications/NotificationRandomArticle.java
@@ -1,57 +1,58 @@
 package org.wikipedia.notifications;
 
-import android.app.PendingIntent;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.BroadcastReceiver;
-import android.widget.Toast;
-
-import com.allyants.notifyme.NotifyMe;
-
-import org.wikipedia.R;
-import org.wikipedia.random.RandomActivity;
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import org.wikipedia.R;
 
 /* This class will display a Notification of a Random Article */
 
 public class NotificationRandomArticle extends BroadcastReceiver {
-    //public class NotificationRandomArticle extends NotificationActivity implements NotificationItemActionsDialog.Callback {
-    private static final String CHANNEL_ID = "RANDOM_ARTICLE_CHANNEL";
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Alarm running", Toast.LENGTH_SHORT).show();
-        Intent recieveIntent = new Intent(context, RandomActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, recieveIntent, 0);
-        createNotificationForRandomArticle(context);
+        String CHANNEL_ID = "my_channel_01";
+        //PendingIntent notification = PendingIntent.getActivity(context,0,new Intent(context, NotificationSchedulerActivity.class),0);
 
-        //NotificationManagerCompat mNotificationManager = NotificationManagerCompat.from(this);
-        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager manager =( NotificationManager ) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+            CharSequence name = "my_channel";
+            String Description = "This is my channel";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(Description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            manager.createNotificationChannel(mChannel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,CHANNEL_ID)
+                .setSmallIcon(R.drawable.w_nav_mark)
+                .setContentTitle("Hey! Want to read something interesting?")
+                .setContentText("Click here to read a random article on Wikipedia.");
+
+        builder.setDefaults(NotificationCompat.DEFAULT_SOUND);
+        builder.setAutoCancel(true);
+
+        Intent resultIntent = new Intent(context, NotificationSchedulerActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(NotificationSchedulerActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(resultPendingIntent);
+        ;
+
+        manager.notify(1, builder.build());
+        //mm.cancel(1);
     }
-
-    public void createNotificationForRandomArticle(Context context) {
-        String etTitle = "Hey! Want to read something interesting?";
-        String etContent = "Click here to read a random article on Wikipedia.";
-        NotifyMe notifyMe = new NotifyMe.Builder(context)
-                .title(etTitle)
-                .content(etContent)
-                .color(255,0,0,255)
-                .led_color(255,255,255,255)
-                //.time(now)
-                //.addAction(new Intent(),"Snooze",false)
-                .key("test")
-                .addAction(new Intent(),"Dismiss",true,false)
-                .addAction(new Intent(context,RandomActivity.class),"Check it out")
-                .large_icon(R.mipmap.launcher)
-                .build();
-    }
-
 }
