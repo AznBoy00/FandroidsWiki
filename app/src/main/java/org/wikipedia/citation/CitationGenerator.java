ff60c8aa07861ce37ec1582520e41f1472385877
TheWolfOfTheNorth <davebhardwaj1@gmail.com>
2019-03-03 19:40:24 -0500

    Created IEEE String Builder for Citation #27 #29

--- app/src/main/java/org/wikipedia/citation/CitationGenerator.java
+++ app/src/main/java/org/wikipedia/citation/CitationGenerator.java
@@ -1,16 +1,23 @@
 package org.wikipedia.citation;
 
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.wikipedia.citation.CitationStyle;
+import org.wikipedia.page.PageBackStackItem;
 
+import java.util.Calendar;
 public class CitationGenerator {
     private String outPutString;
     private final String _WIKIPEDIA = "Wikipedia";
     private String citationDate;
     private String link;
+    private String mobileURL;
     private CitationStyle citStyle;
     private String textFormat;
+    private String currentDate;
 
     public CitationGenerator(){
         outPutString = "";
@@ -27,6 +34,28 @@ public class CitationGenerator {
         this.link = "";
         textFormat = "";
         citStyle = CitationStyle.APA;
+    }
+
+    public CitationGenerator (String URL){
+        mobileURL = URL;
+        setDateTime();
+    }
+
+    public void setDateTime() {
+        DateFormat df = new SimpleDateFormat("d MMM yyyy");
+        this.currentDate = df.format(Calendar.getInstance().getTime());
+    }
+
+    public String IEEECitationGenerator(String ArticleTitle){
+        String citeBuilder = "";
+        citeBuilder += "\"" + ArticleTitle + ",\"";
+        citeBuilder += " Wikipedia,";
+        citeBuilder += currentDate;
+        citeBuilder += ". [Online]. Available: ";
+        citeBuilder += mobileURL;
+        citeBuilder += ". [Accessed: " + currentDate + "].";
+
+        return citeBuilder;
     }
 
     public void setCitationDate(String date)
