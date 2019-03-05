968bd8b70d1cde283f14bcde4a1a2673f0ef47c5
AznBoy00 <Lin_Kevin_1995@hotmail.com>
2019-03-05 16:32:38 -0500

    #36 Implemented QRCode and passed it by using Mockito Spy.

---
+++ app/src/test/java/org/wikipedia/qrcode/QRCodeGenerateTest.java
@@ -0,0 +1,43 @@
+package org.wikipedia.qrcode;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.wikipedia.page.PageBackStackItem;
+import org.wikipedia.page.PageTitle;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+public class QRCodeGenerateTest {
+    private static final String TAG = "QRCodeGenerateTest";
+
+    PageBackStackItem lastTabMock;
+    PageTitle titleMock;
+    AppCompatActivity appCompatActivity;
+    Bitmap qrImage;
+    private String url;
+
+    @Test public void generateQRCodeTest() throws Throwable {
+        //mock
+        appCompatActivity = mock(AppCompatActivity.class);
+        lastTabMock = mock(PageBackStackItem.class);
+        titleMock = mock(PageTitle.class);
+        qrImage = mock(Bitmap.class);
+
+        url = "https://en.wikipedia.org/wiki/Apple_Inc.";
+
+        //stub
+        when(lastTabMock.getTitle()).thenReturn(titleMock);
+        when(titleMock.getCanonicalUri()).thenReturn(url);
+
+        //Spy
+        QRCodeGenerateActivity qrCodeGenerateActivitySpy = Mockito.spy(QRCodeGenerateActivity.class);
+        qrCodeGenerateActivitySpy.generateImage(lastTabMock.getTitle().getCanonicalUri());
+        System.out.println("[" + TAG + "]" + "Mock URL: " + titleMock.getCanonicalUri() + " = String URL: " + url);
+        verify(qrCodeGenerateActivitySpy).generateImage(lastTabMock.getTitle().getCanonicalUri());
+    }
+}
