850a1aa87a2627ef449467fd4e377e85513a8b63
vincent.sun870530@gmail.com <vincent.sun870530@gmail.com>
2019-03-18 14:07:45 -0400

    add a MLservice test #66

--- app/src/test/java/org/wikipedia/mlkit/MLKitServiceTest.java
+++ app/src/test/java/org/wikipedia/mlkit/MLKitServiceTest.java
@@ -1,4 +1,23 @@
 package org.wikipedia.mlkit;
 
+import android.graphics.Bitmap;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
 public class MLKitServiceTest {
+
+    MLKitService mlKitService = MLKitService.getMLKitServiceInstance();
+    @Test
+    public void testimageFormBitmap(){
+
+        Bitmap bitmap = mock(Bitmap.class);
+        MLKitService.imageFromBitmap(bitmap);
+
+    }
+
+
+
+
 }
