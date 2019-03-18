f2c3a163db48099f53e892a3cc9bf1345cb67360
Kevin Lo <lo_kevin@outlook.com>
2019-03-18 17:42:21 -0400

    #67 Added a failed case

--- app/src/test/java/org/wikipedia/googleVision/SearchResultListTest.java
+++ app/src/test/java/org/wikipedia/googleVision/SearchResultListTest.java
@@ -13,6 +13,7 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -27,6 +28,7 @@ public class SearchResultListTest {
     List<String> expectedResultsList;
     List<String> expectedCleanUrlsList;
     String expectedCleanUrls;
+    List<String> fakeExpectedCleanUrlsList;
 
     @Before
     public void SetUp() {
@@ -34,43 +36,25 @@ public class SearchResultListTest {
         WikiUrlRetriever urlRetriever = mock(WikiUrlRetriever.class);
         NetworkUtils urlBuilder = mock(NetworkUtils.class);
         searchResultsMocked = mock(SearchResultsFromGoogleVisionActivity.class);
-        String fakeKeyword = "honda";
-//        Mockito.doReturn(urlBuilder.buildSpecialUrl("honda")).when(searchResultsMocked);
         expectedResults = "[https://en.wikipedia.org/wiki/Honda, https://en.wikipedia.org/wiki/Honda_Civic, https://en.wikipedia.org/wiki/Honda_Accord, https://en.wikipedia.org/wiki/Honda_Civic_Type_R, https://en.wikipedia.org/wiki/Honda_Fit, https://en.wikipedia.org/wiki/Honda_City, https://en.wikipedia.org/wiki/Honda_Gold_Wing, https://en.wikipedia.org/wiki/Honda_in_Formula_One, https://en.wikipedia.org/wiki/Honda_Civic_(sixth_generation), https://en.wikipedia.org/wiki/Honda_Insight]";
-//        expectedResults = "[https://en.wikipedia.org/wiki/Honda]";
         expectedResultsList = Arrays.asList("https://en.wikipedia.org/wiki/Honda", "https://en.wikipedia.org/wiki/Honda_Civic", "https://en.wikipedia.org/wiki/Honda_Accord", "https://en.wikipedia.org/wiki/Honda_Civic_Type_R", "https://en.wikipedia.org/wiki/Honda_Fit", "https://en.wikipedia.org/wiki/Honda_City", "https://en.wikipedia.org/wiki/Honda_Gold_Wing", "https://en.wikipedia.org/wiki/Honda_in_Formula_One", "https://en.wikipedia.org/wiki/Honda_Civic_(sixth_generation)", "https://en.wikipedia.org/wiki/Honda_Insight");
         expectedCleanUrlsList = Arrays.asList("Honda", "Honda Civic", "Honda Accord", "Honda Civic Type R", "Honda Fit", "Honda City", "Honda Gold Wing", "Honda in Formula One", "Honda Civic (sixth generation)", "Honda Insight");
         expectedCleanUrls = "[Honda, Honda Civic, Honda Accord, Honda Civic Type R, Honda Fit, Honda City, Honda Gold Wing, Honda in Formula One, Honda Civic (sixth generation), Honda Insight]";
+        fakeExpectedCleanUrlsList = Arrays.asList("Honda", "Honda Civic", "Honda Accord", "Honda Civic Type R", "Honda Fit", "Honda City", "Honda Gold Wing", "Honda in Formula One", "Honda Civic (sixth generation)", "Honda Insight");
 
-    }
-
-    /** Tests for the display of keywords generated from Google Vision **/
-    @Test
-    public void testDisplayGoogleVisionResults() {
-//        when(searchResults.displayGoogleVisionResults(new String[]{"honda"})).thenReturn(expectedResultsList);
-//        SearchResultsFromGoogleVisionActivity searchResults = new SearchResultsFromGoogleVisionActivity();
-//        SearchResultsFromGoogleVisionActivity searchResults = mock(SearchResultsFromGoogleVisionActivity.class);
-//        NetworkUtils network = Mockito.spy(NetworkUtils.class);
-//        Uri uri = Mockito.spy(Uri.class);
-////        URL url = mock(URL.class);
-////        try {
-//            Mockito.doReturn((uri.parse( "https://en.wikipedia.org/w/api.php?action=opensearch&limit=10&format=json&search=honda").buildUpon().build()).toString()).when(network.buildSpecialUrl("honda"));
-//
-////        } catch (MalformedURLException e) {
-////            e.printStackTrace();
-////        }
-//        List<String> listOfUrls = searchResults.displayGoogleVisionResults(new String[]{"honda"});
-//        System.out.println("TEST:" + listOfUrls.toString());
-//        assertTrue((listOfUrls.toString()).equals(expectedResults));
     }
 
     @Test
     public void TestConvertToTitle() {
-//        when(searchResults.convertToTitle(expectedResultsList)).thenReturn(expectedCleanUrlsList);
         SearchResultsFromGoogleVisionActivity searchResults = new SearchResultsFromGoogleVisionActivity();
         List<String> cleanUrls = searchResults.convertToTitle(expectedResultsList);
-        System.out.println("TEST:" + cleanUrls.toString());
-        System.out.println("TEST:" + expectedCleanUrls.toString());
         assertTrue((cleanUrls.toString()).equals(expectedCleanUrls));
+    }
+
+    @Test
+    public void TestConvertToTitleFalse() {
+        SearchResultsFromGoogleVisionActivity searchResults = new SearchResultsFromGoogleVisionActivity();
+        List<String> cleanUrls = searchResults.convertToTitle(expectedResultsList);
+        assertNotSame(cleanUrls.toString(), fakeExpectedCleanUrlsList);
     }
 }
