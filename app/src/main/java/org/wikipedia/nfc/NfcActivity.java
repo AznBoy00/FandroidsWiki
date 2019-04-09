6f6c952e85fc2bb7cb7c077e47b20b1524a2888c
lokevin88 <lo_kevin@outlook.com>
2019-04-09 17:19:09 -0400

    #94 Fixed problem with back button + removed onclick events for buttons that are now removed

--- app/src/main/java/org/wikipedia/nfc/NfcActivity.java
+++ app/src/main/java/org/wikipedia/nfc/NfcActivity.java
@@ -9,6 +9,8 @@ import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
@@ -23,7 +25,7 @@ import org.wikipedia.page.tabs.Tab;
 
 import java.util.List;
 
-public class NfcActivity extends Activity implements NfcAdapter.CreateNdefMessageCallback {
+public class NfcActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback {
 
     NfcAdapter nfcAdapter;
     String useOfNFC;
@@ -35,10 +37,22 @@ public class NfcActivity extends Activity implements NfcAdapter.CreateNdefMessag
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
 
-//        Button buttonSend = findViewById(R.id.button_share);
-//        Button buttonReceive = findViewById(R.id.button_receive);
+        // Declarations
         webView = (WebView) findViewById(R.id.webview);
         nfcFrameLayout = findViewById(R.id.nfc_frame_layout);
+        Toolbar toolbar = findViewById(R.id.page_toolbar);
+
+        // Set the action bar and when clicked (Wikipedia back button), will finish the activity
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Intent intent = getIntent();
+        useOfNFC = intent.getStringExtra("for");
 
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (nfcAdapter == null) {
@@ -50,34 +64,7 @@ public class NfcActivity extends Activity implements NfcAdapter.CreateNdefMessag
             Toast.makeText(this, "Please enable NFC.", Toast.LENGTH_LONG).show();
         }
 
-        Intent intent = getIntent();
-        useOfNFC = intent.getStringExtra("for");
-    }
-
-    /**
-     * This method is called when clicked on the Share Article button
-     * When the user clicks on the Share Article button, it will send the link via NFC
-     */
-    public void sendArticle(View view) {
-//        WikipediaApp app = WikipediaApp.getInstance();
-//        Tab currentTab = app.getTabList().get(app.getTabList().size() - 1);
-//        PageBackStackItem lastTab = currentTab.getBackStack().get(currentTab.getBackStackPosition());
-//        String url = lastTab.getTitle().getCanonicalUri();
-//        Intent i = new Intent(Intent.ACTION_VIEW);
-//        i.setData(Uri.parse(url));
-
-        Toast.makeText(this, "Sending article.", Toast.LENGTH_LONG).show();
         nfcAdapter.setNdefPushMessageCallback(this, this);
-    }
-
-    /**
-     * This method is called when clicked on the Receive Article button
-     * When the user clicks on the Receive Article button, it will listen to NFC messages
-     * sent from nearby devices
-     */
-    public void receiveArticle(View view) {
-        Toast.makeText(this, "Waiting to receive article.", Toast.LENGTH_LONG).show();
-        onResume();
     }
 
     @Override
@@ -88,9 +75,8 @@ public class NfcActivity extends Activity implements NfcAdapter.CreateNdefMessag
             Tab currentTab = app.getTabList().get(app.getTabList().size() - 1);
             PageBackStackItem lastTab = currentTab.getBackStack().get(currentTab.getBackStackPosition());
             data = useOfNFC + "," + lastTab.getTitle().getCanonicalUri();
-//            return new NdefMessage(new NdefRecord[] { NdefRecord.createUri(url) });
         } else if(useOfNFC.equals("note")) {
-            //DO SOMETHING
+            //DO SOMETHING FOR NOTES
         }
         NdefRecord ndefRecord = NdefRecord.createMime("text/plain", data.getBytes());
         NdefMessage message = new NdefMessage(ndefRecord);
@@ -99,37 +85,39 @@ public class NfcActivity extends Activity implements NfcAdapter.CreateNdefMessag
 
     @Override
     public void onResume() {
-        Toast.makeText(this, "Scanning.", Toast.LENGTH_LONG).show();
         super.onResume();
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
             showMessage(getIntent());
         }
     }
 
-
     @Override
     public void onNewIntent(Intent intent) {
         setIntent(intent);
     }
 
-
     public void showMessage(Intent intent) {
         Parcelable[] message = intent.getParcelableArrayExtra(
                 NfcAdapter.EXTRA_NDEF_MESSAGES);
         NdefMessage nfcMessage = (NdefMessage) message[0];
-
         String rawData = new String(nfcMessage.getRecords()[0].getPayload());
-        Log.d("TEST", rawData);
         String[] data = rawData.split(",");
-        Log.d("TEST2", "data have a size of " + (data.length));
         if(data[0].equals("article")) {
             String url = data[1];
-            Log.d("TEST3", url);
-            Toast.makeText(this, url, Toast.LENGTH_LONG);
             nfcFrameLayout.setVisibility(View.GONE);
             webView.setVisibility(View.VISIBLE);
             webView.loadUrl(url);
+        } else if(data[0].equals("note")) {
+            // DO SOMETHING FOR NOTES
         }
+    }
+
+    /**
+     * Will close the activity when pressed on back button (Android)
+     */
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 
 }
