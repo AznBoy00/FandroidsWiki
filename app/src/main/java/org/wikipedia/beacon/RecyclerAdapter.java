8fb44079784cda723668af98783cdc9b30546bc2
Yefei Xue <felixyf0124@gmail.com>
2019-04-11 12:35:39 -0400

    #106 spot wiki from beacon clickable

--- app/src/main/java/org/wikipedia/beacon/RecyclerAdapter.java
+++ app/src/main/java/org/wikipedia/beacon/RecyclerAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -89,6 +90,13 @@ public class RecyclerAdapter  extends RecyclerView.Adapter<RecyclerAdapter.ViewH
                     if (dataSnapshot.hasChild(""+_uuid)) {
                         final String wikiSpotName = dataSnapshot.child(""+_uuid).child("name").getValue().toString();
                         holder.wikiSpot.setText(wikiSpotName);
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v){
+                                Toast.makeText(v.getContext(), "Spot Wiki "+ wikiSpotName, Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }else {
                         holder.wikiSpot.setText("Non-Wiki Spot");
                     }
