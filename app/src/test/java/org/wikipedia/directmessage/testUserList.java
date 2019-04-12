1ab144c2f9a1c0f7a39eb2f5764a763760bf8f0b
WolfOfTheNorth <davebhardwaj@gmail.com>
2019-04-11 21:51:51 -0400

    fixed unit test #108

--- app/src/test/java/org/wikipedia/directmessage/testUserList.java
+++ app/src/test/java/org/wikipedia/directmessage/testUserList.java
@@ -2,54 +2,49 @@ package org.wikipedia.directmessage;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.powermock.api.mockito.PowerMockito;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertArrayEquals;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class testUserList {
 
-    Context context;
-    Intent intent;
-    String request;
-    JSONObject jsonObject;
-    Iterator iterator;
-    String key;
-    ArrayList<String> mockArrayList;
 
     @Before
-    public void SetUp() {
-        context = PowerMockito.mock(Context.class);
-        intent = mock(Intent.class);
-        iterator = mock(Iterator.class);
-        request = "{\"John Smith\":{\"email\":\"johnsmith1@mail.com\",\"username\":\"John Smith\"},\"Jane Doe\":{\"email\":\"janedoe@mail.com\",\"username\":\"Jane Doe\"}}";
-
-
-    }
+    public void SetUp() {}
 
     @Test
     public void testOnSuccess() throws Exception{
-        jsonObject = mock(JSONObject.class);
-        PowerMockito.whenNew(JSONObject.class).withArguments(request).thenReturn(jsonObject);
-        iterator = jsonObject.keys();
-        mockArrayList = mock(ArrayList.class);
-
-        while(iterator.hasNext()){
-            key = iterator.next().toString();
-            assertNotEquals(iterator.toString(), iterator.next().toString());
-            mockArrayList.add(key);
-            verify(mockArrayList).add(key);
+        Iterator<Integer> mockIterator = mock(Iterator.class);
+        when(mockIterator.hasNext()).thenReturn(true, true, true, false);
+        Integer[] keys = new Integer [] {1, 2, 3, 4};
+        when(mockIterator.next()).thenReturn(keys[0], keys[1], keys[2], keys[3]);
+        ArrayList<Integer> mockArrayList = mock(ArrayList.class);
+        int index = 0;
+        while(mockIterator.hasNext()) {
+            int keyValue = keys[index];
+            int iteratorValue = mockIterator.next();
+            assertEquals(keyValue, iteratorValue);
+            if(keys[0] != iteratorValue){
+                mockArrayList.add(iteratorValue);
+                assertTrue(keys[0] != iteratorValue);
+            }
+            index++;
         }
-
     }
 
 
