497dbbc9b45bec285beed9087aba818eb1b84155
WolfOfTheNorth <davebhardwaj@gmail.com>
2019-04-12 23:36:37 -0400

    #108 added User List edge case

--- app/src/test/java/org/wikipedia/directmessage/testUserList.java
+++ app/src/test/java/org/wikipedia/directmessage/testUserList.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -19,17 +20,23 @@ public class testUserList {
         when(mockIterator.next()).thenReturn(keys[0], keys[1], keys[2], keys[3]);
         ArrayList<Integer> mockArrayList = mock(ArrayList.class);
         int index = 0;
+        /**
+         * Here we want to assume '1' is like our logged in user
+         * and thus do NOT want to have it in the arrayList which
+         * holds the list of users that '1' can message.
+         */
+        int currentUser = keys[0];
         while(mockIterator.hasNext()) {
             int keyValue = keys[index];
             int iteratorValue = mockIterator.next();
             assertEquals(keyValue, iteratorValue);
-            if(keys[0] != iteratorValue){
+            if(currentUser != iteratorValue){
                 mockArrayList.add(iteratorValue);
-                assertTrue(keys[0] != iteratorValue);
+                assertTrue(currentUser != iteratorValue);
             }
+            // Edge Case : check that currentUser is not in mockArrayList
+            assertFalse(mockArrayList.contains(currentUser));
             index++;
         }
     }
-
-
 }
