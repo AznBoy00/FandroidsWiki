7a177bc1a297b6af21848a3d599ba4a84b443535
WolfOfTheNorth <davebhardwaj@gmail.com>
2019-04-12 23:36:11 -0400

    #108 Added edge case test for chat

--- app/src/test/java/org/wikipedia/directmessage/testChat.java
+++ app/src/test/java/org/wikipedia/directmessage/testChat.java
@@ -4,6 +4,7 @@ import android.widget.TextView;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -12,9 +13,14 @@ public class testChat {
     @Test
     public void testAddMessageBox() {
         String testMessage = "Message";
+        Object mockObject = mock(Object.class);
 
         TextView mockTextView = mock(TextView.class);
         when(mockTextView.getText()).thenReturn("Message");
         assertEquals(testMessage, mockTextView.getText());
+
+        // Edge case: Check that message isn't unnecessarily large
+        when(mockTextView.getText().length()).thenReturn(99999);
+        assertTrue(mockTextView.getText().length() <= 999999999);
     }
 }
